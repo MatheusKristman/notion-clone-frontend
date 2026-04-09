8b6216908e83deb83ad19a7bd733c904c491c5b6
MatheusKristman <kristman058@gmail.com>
2026-04-09 10:47:26 -0300

    feat: base mobile header structure

--- app/(website)/components/header.tsx
+++ app/(website)/components/header.tsx
@@ -1,7 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { MenuIcon } from "lucide-react";
+import Image from "next/image";
+
 export const Header = () => {
     return (
-        <header className="w-full sticky px-4 py-3.5 bg-blue-500">
-            <div>Header</div>
+        <header className="w-full sticky px-4 py-3.5 bg-transparent flex items-center justify-between gap-4">
+            <Image src="/header/Notion-logo.svg" alt="Notion Logo" width={33} height={34} />
+
+            <div className="w-fit flex items-center gap-2">
+                <Button>Use o Notion de graça</Button>
+
+                <Button size="icon" variant="ghost" className="size-11">
+                    <MenuIcon className="w-6! h-auto! flex" />
+                </Button>
+            </div>
         </header>
     );
 };
